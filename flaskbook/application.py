340fd754832ec13153201bd180d5a19ac579afe8
GuilleCM <guille.mca@hotmail.com>
2017-08-17 14:41:16 +0000

    creado model.py para user y creado modulo utilities

--- flaskbook/application.py
+++ flaskbook/application.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+    
+    #init db
+    db.init_app(app)
     
     #Adding blueprints
     from user.views import user_app
