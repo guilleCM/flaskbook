2a90b40a17f77e9073316980f08333f8137cf11d
GuilleCM <guille.mca@hotmail.com>
2017-08-18 10:18:02 +0000

    añadida vista login y sus casos test

--- flaskbook/user/views.py
+++ flaskbook/user/views.py
@@ -10,6 +10,26 @@ user_app = Blueprint('user_app', __name__)
 def login():
     form = LoginForm()
     error = None
+    if request.method == 'GET' and request.args.get('next'):
+        session['next'] = request.args.get('next')
+        
+    if form.validate_on_submit():
+        user = User.objects.filter(
+            username=form.username.data
+            ).first()
+        if user:
+            if bcrypt.hashpw(form.password.data, user.password) == user.password:
+                session['username'] = form.username.data
+                if 'next' in session:
+                    next = session.get('next')
+                    session.pop('next')
+                    return redirect(next)
+                else:
+                    return 'User logged in'
+            else:
+                user = None
+        if not user:
+            error = 'Incorrect credentials'
     return render_template('user/login.html',
                             form=form,
                             error=error)
