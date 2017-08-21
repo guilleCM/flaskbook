e9e06f848e68698a09b19148abe476cf6b620815
GuilleCM <guille.mca@hotmail.com>
2017-08-21 14:19:47 +0000

    añadida configuracion para que las imagenes suban a aws s3

--- flaskbook/settings.py
+++ flaskbook/settings.py
@@ -4,3 +4,11 @@ MONGODB_DB = 'flaskbook'
 HOSTNAME = 'https://flaskbook-guillecm.c9users.io'
 UPLOAD_FOLDER = '/home/ubuntu/workspace/flaskbook/static/images'
 STATIC_IMAGE_URL = 'images'
+
+# uncomment por production
+# AWS_BUCKET = 'myflaskapp'
+# AWS_CONTENT_URL = 'https://s3-eu-west-1.amazonaws.com'
+
+# for dev
+AWS_BUCKET = ''
+AWS_CONTENT_URL = ''
--- flaskbook/user/models.py
+++ flaskbook/user/models.py
@@ -5,7 +5,7 @@ import os
 # APP MODULES
 from application import db
 from utilities.common import utc_now_ts as now
-from settings import STATIC_IMAGE_URL
+from settings import STATIC_IMAGE_URL, AWS_BUCKET, AWS_CONTENT_URL
 
 class User(db.Document):
     username = db.StringField(db_field="u", required=True, unique=True)
@@ -25,7 +25,10 @@ class User(db.Document):
         document.email = document.email.lower()
     
     def profile_imgsrc(self, size):
-        return os.path.join(STATIC_IMAGE_URL, 'user', '%s.%s.%s.png' % (self.id, self.profile_image, size) )
+        if AWS_BUCKET:
+            return os.path.join(AWS_CONTENT_URL, AWS_BUCKET, 'user', '%s.%s.%s.png' % (self.id, self.profile_image, size) )
+        else:
+            return url_for('static', filename=os.path.join(STATIC_IMAGE_URL, 'user', '%s.%s.%s.png' % (self.id, self.profile_image, size) ))
         
     meta = {
         'indexes': ['username', 'email', '-created'] #poniendo el - delante decimos que sea en orden inverso (los mas recientes primero en el caso del campo created)
